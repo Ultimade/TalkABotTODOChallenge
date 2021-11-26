6b8f0db8a09b1b1ee8ac0ac752f91071dde383cf
zsolt <WZsolt90@bitbucket.org>
2021-11-26 09:22:18 +0100

    Basic init empty project

---
+++ src/main/java/hu/talkabot/TalkABotTODOChallenge/TalkABotTodoChallengeApplication.java
@@ -0,0 +1,13 @@
+package hu.talkabot.TalkABotTODOChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkABotTodoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkABotTodoChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/talkabot/TalkABotTODOChallenge/TalkABotTodoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.talkabot.TalkABotTODOChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkABotTodoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
